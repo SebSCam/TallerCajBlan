63aba7d77c2cceab15df2ece09c37b21f158314e
Fabian-Ojeda <fabian.ojeda01@uptc.edu.co>
2022-10-15 18:27:59 -0500

    Estoy intentando usar git dede eclipse ¯\_(o_O)_/¯

---
+++ src/co/edu/uptc/test/TestCalificacion.java
@@ -0,0 +1,14 @@
+package co.edu.uptc.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCalificacion {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
