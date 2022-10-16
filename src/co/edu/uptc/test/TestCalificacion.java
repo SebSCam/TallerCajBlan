f57a33c022bd60e05edf74487348bde2db897444
Fabian-Ojeda <fabian.ojeda01@uptc.edu.co>
2022-10-15 19:42:56 -0500

    Agregación de otras pruebas de calificación

--- src/co/edu/uptc/test/TestCalificacion.java
+++ src/co/edu/uptc/test/TestCalificacion.java
@@ -8,10 +8,34 @@ class TestCalificacion {
 
 	@Test
 	void testExcelente() {
-		System.out.println("Inicio de la prueba de calificación para el nivel Excelente ");
+		System.out.println("Inicio de la prueba de calificación para el nivel Excelente :D");
 		Calificacion calificacion = new Calificacion();
 		String resultado = calificacion.calificacion(10, 9, 8);
-		assertEquals(resultado, "Excelente");
+		assertEquals("Excelente", resultado);
+	}
+	
+	@Test
+	void testBuena() {
+		System.out.println("Inicio de la prueba de calificación para el nivel Buena :)");
+		Calificacion calificacion = new Calificacion();
+		String resultado = calificacion.calificacion(8, 8, 8);
+		assertEquals("Buena", resultado);
+	}
+	
+	@Test
+	void testAceptable() {
+		System.out.println("Inicio de la prueba de calificación para el nivel Aceptable :|");
+		Calificacion calificacion = new Calificacion();
+		String resultado = calificacion.calificacion(7, 7, 7);
+		assertEquals("Aceptable", resultado);
+	}
+	
+	@Test
+	void testReprobado() {
+		System.out.println("Inicio de la prueba de calificación para el nivel Reprobado :(");
+		Calificacion calificacion = new Calificacion();
+		String resultado = calificacion.calificacion(1, 5, 3);
+		assertEquals("Reprobado", resultado);
 	}
 
 }
