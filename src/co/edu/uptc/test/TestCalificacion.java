62e967c122eac1d59eaeaaf1edcd8842d2620a20
Fabian-Ojeda <fabian.ojeda01@uptc.edu.co>
2022-10-15 19:31:50 -0500

    Se fue la primer prueba

--- src/co/edu/uptc/test/TestCalificacion.java
+++ src/co/edu/uptc/test/TestCalificacion.java
@@ -1,14 +1,17 @@
 package co.edu.uptc.test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import co.edu.uptc.classes.*;
 
 class TestCalificacion {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testExcelente() {
+		System.out.println("Inicio de la prueba de calificación para el nivel Excelente ");
+		Calificacion calificacion = new Calificacion();
+		String resultado = calificacion.calificacion(10, 9, 8);
+		assertEquals(resultado, "Excelente");
 	}
 
 }
